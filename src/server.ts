393b7c1d3ebc5f9864703b669e6048826db520d2
Nic Sarkis <nic.sarkis@synergy55.com>
2024-08-07 09:39:16 -0500

    nodemon

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ const express = require('express');
 import { Request, Response } from 'express';
 
 import { checkDatabaseConnection } from './database';
-import { createRole } from './controller/matrix/mtx_role/create'
 import indexRoute from './routes';
 
 const app = express();
@@ -18,24 +17,9 @@ app.get("/api/ping", async (req: Request, res: Response) => {
     }
 });
 
-// app.get("/api/createRole", async (req: Request, res: Response) => {
-//     try {
-//         const data = {
-//             query: {
-//                 name: "Nic",
-//                 descriptions: "desc",
-//                 createdAt: "2023-07-03T12:00:00Z",
-//                 enabled: "true"
-//             }
-//         };
-//         const role = await createRole(data);
-//         res.status(500).send("Role: " + role);
-//     } catch (error) {
-//         res.status(500).send("Error " + error);
-//     }
-// });
-
 checkDatabaseConnection();
+
+
 
 app.listen(port, () => {
     console.log(`app listening on port ${port}`);
